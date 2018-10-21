ca2f1fc9ea1450e882f3c23a18c5239e8742d081
amirnakic <anakic1@etf.unsa.ba>
2018-10-21 17:31:37 +0200

    Kreiranje određenih metoda unutar klase Student.java

--- src/rpr/etf/unsa/ba/Student.java
+++ src/rpr/etf/unsa/ba/Student.java
@@ -1,4 +1,16 @@
 package rpr.etf.unsa.ba;
 
 public class Student {
+    private String ime, prezime;
+    private int brojIndeksa;
+    Student (String ime, String prezime, int br_indeksa) {
+        this.ime=ime;
+        this.prezime=prezime;
+        this.brojIndeksa=br_indeksa;
+    }
+    public String DajIme() { return ime; }
+    public String DajPrezime() { return prezime; }
+    public int DajBrojIndeksa() { return brojIndeksa; }
+    public void PromijeniIme(String ime) { this.ime=ime; }
+    public void PromijeniPrezime(String prezime) { this.prezime=prezime; }
 }
