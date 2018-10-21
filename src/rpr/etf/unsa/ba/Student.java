52d78175ab8c818438652710d50c4cdb113110ff
amirnakic <anakic1@etf.unsa.ba>
2018-10-21 17:42:18 +0200

    Kreiranje metoda unutar klase Student.java i definisanje atributa brojIndeksa kao omotač primitivnog tipa int.

--- src/rpr/etf/unsa/ba/Student.java
+++ src/rpr/etf/unsa/ba/Student.java
@@ -2,15 +2,21 @@ package rpr.etf.unsa.ba;
 
 public class Student {
     private String ime, prezime;
-    private int brojIndeksa;
+    private class IntWrapper {
+        private int value;
+        IntWrapper(int value) { this.value = value; }
+        int PrebaciUInteger() { return (int)this.value; }
+    }
+    private IntWrapper brojIndeksa;
     Student (String ime, String prezime, int br_indeksa) {
-        this.ime=ime;
-        this.prezime=prezime;
-        this.brojIndeksa=br_indeksa;
+        this.ime = ime;
+        this.prezime = prezime;
+        this.brojIndeksa = new IntWrapper(br_indeksa);
     }
     public String DajIme() { return ime; }
     public String DajPrezime() { return prezime; }
-    public int DajBrojIndeksa() { return brojIndeksa; }
-    public void PromijeniIme(String ime) { this.ime=ime; }
+    public int DajBrojIndeksa() { return brojIndeksa.PrebaciUInteger(); }
+    public void PromijeniIme(String ime) { this.ime = ime; }
     public void PromijeniPrezime(String prezime) { this.prezime=prezime; }
+    public void PromijeniBrojIndeksa(int broj) { this.brojIndeksa = new IntWrapper(broj); }
 }
