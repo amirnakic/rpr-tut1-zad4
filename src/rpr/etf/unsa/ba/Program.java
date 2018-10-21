3dafc1b76bb11fc9e933b6bccc59dba09b20906b
amirnakic <anakic1@etf.unsa.ba>
2018-10-21 18:39:48 +0200

    Testiranje metoda klase Student.java.

--- src/rpr/etf/unsa/ba/Program.java
+++ src/rpr/etf/unsa/ba/Program.java
@@ -3,6 +3,11 @@ package rpr.etf.unsa.ba;
 public class Program {
 
     public static void main(String[] args) {
-	// write your code here
+	    Student s = new Student("Amir", "Nakić", 17787 );
+	    s.IspisiStudenta();
+	    s.PromijeniIme("Amila");
+	    s.PromijeniPrezime("Šoše");
+	    s.PromijeniBrojIndeksa(17783);
+	    s.IspisiStudenta();
     }
 }
--- src/rpr/etf/unsa/ba/Student.java
+++ src/rpr/etf/unsa/ba/Student.java
@@ -20,7 +20,7 @@ public class Student {
     public void PromijeniPrezime(String prezime) { this.prezime=prezime; }
     public void PromijeniBrojIndeksa(int broj) { this.brojIndeksa = new IntWrapper(broj); }
     public void IspisiStudenta() {
-        System.out.println(DajPrezime() + DajIme() + DajBrojIndeksa());
+        System.out.println(DajPrezime() + " " + DajIme() + " (" + DajBrojIndeksa() + ")");
     }
     public boolean IstiStudent(Student s) {
         if (this.DajIme().equals(s.DajIme()) && this.DajPrezime().equals(s.DajPrezime()) && this.DajBrojIndeksa() == s.DajBrojIndeksa()) return true;
