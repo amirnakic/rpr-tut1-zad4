c5fa29b165713584148261409cd0bfcb1e837bfb
amirnakic <anakic1@etf.unsa.ba>
2018-10-21 17:59:05 +0200

    Dodavanje atributa i konstruktora klase Predmet.java.

--- src/rpr/etf/unsa/ba/Predmet.java
+++ src/rpr/etf/unsa/ba/Predmet.java
@@ -1,4 +1,12 @@
 package rpr.etf.unsa.ba;
 
 public class Predmet {
+    private String nazivPredmeta, sifraPredmeta;
+    private final int MAX_BROJ_STUDENATA;
+    private Student[] niz;
+    Predmet (String nazivPredmeta, String sifraPredmeta, int brojStudenata) {
+        this.nazivPredmeta = nazivPredmeta; this.sifraPredmeta = sifraPredmeta;
+        this.MAX_BROJ_STUDENATA = brojStudenata;
+        this.niz = new Student[brojStudenata];
+    }
 }
