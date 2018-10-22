88d54adf341238d30bb3fb2c9ec13f7147e884a2
amirnakic <anakic1@etf.unsa.ba>
2018-10-23 00:16:57 +0200

    Dodavanje metoda klasi SviPredmeti.java.

--- src/rpr/etf/unsa/ba/SviPredmeti.java
+++ src/rpr/etf/unsa/ba/SviPredmeti.java
@@ -4,8 +4,20 @@ public class SviPredmeti {
     private Predmet[] niz;
     private final int MAX_BROJ_PREDMETA;
     private int trenutniBrojPredmeta = 0;
-    SviPredmeti(int MAX_BROJ_PREDMETA) {
-        niz[] = new Predmet[MAX_BROJ_PREDMETA];
+    SviPredmeti(int brojPredmeta) {
+        this.MAX_BROJ_PREDMETA = brojPredmeta;
+        this.niz = new Predmet[brojPredmeta];
     }
-    
+    public void DodajPredmet(Predmet p) {
+        niz[trenutniBrojPredmeta] = new Predmet(p.DajNazivPredmeta(), p.DajSifruPredmeta(), p.DajMaxBrojStudenata());
+        trenutniBrojPredmeta++;
+    }
+    public void BrisiPredmet(Predmet p) {
+        for (int i = 0; i < MAX_BROJ_PREDMETA; i++) {
+            if (p.DajNazivPredmeta().equals(niz[i].DajNazivPredmeta()) && p.DajSifruPredmeta().equals(niz[i].DajSifruPredmeta()) && p.DajMaxBrojStudenata() == niz[i].DajMaxBrojStudenata()) {
+                niz[i] = null;
+                trenutniBrojPredmeta--;
+            }
+        }
+    }
 }
