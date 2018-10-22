646b4596ecc3e2095c156acba30e9aadda3d8e34
amirnakic <anakic1@etf.unsa.ba>
2018-10-23 00:08:12 +0200

    Dodavanje konstruktora klasi SviPredmeti.java.

--- src/rpr/etf/unsa/ba/SviPredmeti.java
+++ src/rpr/etf/unsa/ba/SviPredmeti.java
@@ -2,5 +2,10 @@ package rpr.etf.unsa.ba;
 
 public class SviPredmeti {
     private Predmet[] niz;
+    private final int MAX_BROJ_PREDMETA;
     private int trenutniBrojPredmeta = 0;
+    SviPredmeti(int MAX_BROJ_PREDMETA) {
+        niz[] = new Predmet[MAX_BROJ_PREDMETA];
+    }
+    
 }
