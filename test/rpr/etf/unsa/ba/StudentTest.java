f530dfa6e316ff596b65f4268055d27ab8aee72c
amirnakic <anakic1@etf.unsa.ba>
2018-10-23 13:50:57 +0200

    Dodavanje testne klase za klasu Student.java.

---
+++ test/rpr/etf/unsa/ba/StudentTest.java
@@ -0,0 +1,44 @@
+package rpr.etf.unsa.ba;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    @Test
+    void dajIme() {
+    }
+
+    @Test
+    void dajPrezime() {
+    }
+
+    @Test
+    void dajBrojIndeksa() {
+    }
+
+    @Test
+    void promijeniIme() {
+    }
+
+    @Test
+    void promijeniPrezime() {
+    }
+
+    @Test
+    void promijeniBrojIndeksa() {
+    }
+
+    @Test
+    void ispisiStudenta() {
+    }
+
+    @Test
+    void istiStudent() {
+    }
+
+    @Test
+    void brisiStudenta() {
+    }
+}
