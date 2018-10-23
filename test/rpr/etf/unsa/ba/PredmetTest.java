dc523d60f7625e099c5d61a74dd9957ba0ac4927
amirnakic <anakic1@etf.unsa.ba>
2018-10-23 14:28:22 +0200

    Kreiranje klase PredmetTest.java za testiranje metoda klase Predmet.java.

---
+++ test/rpr/etf/unsa/ba/PredmetTest.java
@@ -0,0 +1,28 @@
+package rpr.etf.unsa.ba;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PredmetTest {
+    @Test
+    void dajNazivPredmeta() {
+    }
+    @Test
+    void dajSifruPredmeta() {
+    }
+    @Test
+    void dajMaxBrojStudenata() {
+    }
+    @Test
+    void upisiStudenta() {
+    }
+    @Test
+    void ispisiStudenta() {
+    }
+    @Test
+    void ispisSpiskaStudenata() {
+    }
+    @Test
+    void brisiPredmet() {
+    }
+}
