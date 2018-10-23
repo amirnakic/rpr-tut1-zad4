af6d04c5adb8ffa32881528b5c30769c58182959
amirnakic <anakic1@etf.unsa.ba>
2018-10-23 14:06:09 +0200

    Dodavanje sadržaja određenih testova u klasi StudentTest.java.

--- test/rpr/etf/unsa/ba/StudentTest.java
+++ test/rpr/etf/unsa/ba/StudentTest.java
@@ -1,23 +1,32 @@
 package rpr.etf.unsa.ba;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudentTest {
-
+    @Test
+    void testKonstruktora() {
+        Student s = new Student ("Amila", "Šoše", 17783);
+        assertAll("grupni test za funkcionalnost konstruktora",
+            () ->  assertEquals("Amila", s.DajIme()),
+            () -> assertEquals("Šoše", s.DajPrezime()),
+            () -> assertEquals(17783, s.DajBrojIndeksa()));
+    }
     @Test
     void dajIme() {
+        Student s = new Student ("Amir", "Nakić", 17787);
+        assertEquals("Amir", s.DajIme());
     }
-
     @Test
     void dajPrezime() {
+        Student s = new Student ("Amir", "Nakić", 17787);
+        assertEquals("Nakić", s.DajPrezime());
     }
-
     @Test
     void dajBrojIndeksa() {
+        Student s = new Student("Amir", "Nakić", 17787);
+        assertEquals(17787, s.DajBrojIndeksa());
     }
-
     @Test
     void promijeniIme() {
     }
