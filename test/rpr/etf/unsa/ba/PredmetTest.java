da1e35527372db2dd9d6c6f19d0aefc7e657b2ab
amirnakic <anakic1@etf.unsa.ba>
2018-10-29 09:46:44 +0100

    Reformatiranje koda klase PredmetTest.java.

--- test/rpr/etf/unsa/ba/PredmetTest.java
+++ test/rpr/etf/unsa/ba/PredmetTest.java
@@ -1,6 +1,7 @@
 package rpr.etf.unsa.ba;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PredmetTest {
@@ -9,42 +10,48 @@ class PredmetTest {
         Predmet p = new Predmet("Razvoj programskih rješenja", "RPRAiE2018", 100);
         assertEquals("Razvoj programskih rješenja", p.DajNazivPredmeta());
     }
+
     @Test
     void ispravnostPostavljanjaSifrePredmeta() {
         Predmet p = new Predmet("Razvoj programskih rješenja", "RPRAiE2018", 100);
         assertEquals("RPRAiE2018", p.DajSifruPredmeta());
     }
+
     @Test
     void ispravnostPostavljanjaMaxBrojaStudenata() {
         Predmet p = new Predmet("Razvoj programskih rješenja", "RPRAiE2018", 100);
         assertEquals(100, p.DajMaxBrojStudenata());
     }
+
     @Test
     void ispravnostupisaStudenta() {
         Predmet p = new Predmet("Razvoj programskih rješenja", "RPRAiE2018", 100);
-        Student s = new Student ("Amila", "Šoše", 17783);
+        Student s = new Student("Amila", "Šoše", 17783);
         p.UpisiStudenta(s);
         assertEquals(1, p.DajTrenutniBrojStudenata());
     }
+
     @Test
     void ispisiStudenta() {
         Predmet p = new Predmet("Razvoj programskih rješenja", "RPRAiE2018", 100);
-        Student s1 = new Student ("Amir", "Nakić", 17787);
-        Student s2 = new Student ("Amila", "Šoše", 17783);
+        Student s1 = new Student("Amir", "Nakić", 17787);
+        Student s2 = new Student("Amila", "Šoše", 17783);
         p.UpisiStudenta(s1);
         p.UpisiStudenta(s2);
         p.IspisiStudenta(s1);
         assertEquals(1, p.DajTrenutniBrojStudenata());
     }
+
     @Test
     void ispisSpiskaStudenata() {
         Predmet p = new Predmet("Razvoj programskih rješenja", "RPRAiE2018", 100);
-        Student s1 = new Student ("Amir", "Nakić", 17787);
-        Student s2 = new Student ("Amila", "Šoše", 17783);
+        Student s1 = new Student("Amir", "Nakić", 17787);
+        Student s2 = new Student("Amila", "Šoše", 17783);
         p.UpisiStudenta(s1);
         p.UpisiStudenta(s2);
         assertEquals("1. Nakić Amir (17787)\n2. Šoše Amila (17783)\n", p.IspisSpiskaStudenata());
     }
+
     @Test
     void brisiPredmet() {
         Predmet p = new Predmet("Razvoj programskih rješenja", "RPRAiE2018", 100);
