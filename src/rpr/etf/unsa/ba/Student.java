4248a56ad1a7cba3315a84334233d7c5c1613f52
amirnakic <anakic1@etf.unsa.ba>
2018-10-29 09:45:27 +0100

    Reformatiranje koda klase Student.java.

--- src/rpr/etf/unsa/ba/Student.java
+++ src/rpr/etf/unsa/ba/Student.java
@@ -2,32 +2,65 @@ package rpr.etf.unsa.ba;
 
 public class Student {
     private String ime, prezime;
+
     private class IntWrapper {
         private int value;
-        IntWrapper(int value) { this.value = value; }
-        int PrebaciUInteger() { return (int)value; }
+
+        IntWrapper(int value) {
+            this.value = value;
+        }
+
+        int PrebaciUInteger() {
+            return (int) value;
+        }
     }
+
     private IntWrapper brojIndeksa;
-    Student (String ime, String prezime, int br_indeksa) {
+
+    Student(String ime, String prezime, int br_indeksa) {
         this.ime = ime;
         this.prezime = prezime;
         this.brojIndeksa = new IntWrapper(br_indeksa);
     }
-    public String DajIme() { return ime; }
-    public String DajPrezime() { return prezime; }
-    public int DajBrojIndeksa() { return brojIndeksa.PrebaciUInteger(); }
-    public void PromijeniIme(String ime) { this.ime = ime; }
-    public void PromijeniPrezime(String prezime) { this.prezime=prezime; }
-    public void PromijeniBrojIndeksa(int broj) { this.brojIndeksa = new IntWrapper(broj); }
+
+    public String DajIme() {
+        return ime;
+    }
+
+    public String DajPrezime() {
+        return prezime;
+    }
+
+    public int DajBrojIndeksa() {
+        return brojIndeksa.PrebaciUInteger();
+    }
+
+    public void PromijeniIme(String ime) {
+        this.ime = ime;
+    }
+
+    public void PromijeniPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public void PromijeniBrojIndeksa(int broj) {
+        this.brojIndeksa = new IntWrapper(broj);
+    }
+
     public String IspisiStudenta() {
         String s = this.DajPrezime() + " " + this.DajIme() + " (" + this.DajBrojIndeksa() + ")";
         return s;
     }
+
     public boolean IstiStudent(Student s) {
-        if (this.DajIme().equals(s.DajIme()) && this.DajPrezime().equals(s.DajPrezime()) && this.DajBrojIndeksa() == s.DajBrojIndeksa()) return true;
+        if (this.DajIme().equals(s.DajIme()) && this.DajPrezime().equals(s.DajPrezime()) && this.DajBrojIndeksa() == s.DajBrojIndeksa())
+            return true;
         return false;
     }
+
     public void BrisiStudenta() {
-        ime = null; prezime = null; brojIndeksa = null;
+        ime = null;
+        prezime = null;
+        brojIndeksa = null;
     }
 }
